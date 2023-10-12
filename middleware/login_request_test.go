968223e4b1be71d3c05cc376e5508884f29add6c
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2023-10-12 17:44:24 +0100

    adding unit test

--- middleware/login_request_test.go
+++ middleware/login_request_test.go
@@ -49,3 +49,32 @@ func TestMiddlewareReturnsUnauthorisedIfSessionCookieBlank(t *testing.T) {
 		t.Error(w.Code)
 	}
 }
+
+
+// Ensure that the "sessionid" cookie is not blank in the incoming request
+// the middleware should return an unauthorized response if not.
+func TestMiddlewareAllowsAuthentication(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	t.Setenv("GINDJANGOAUTHTEST", "true")
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Request, _ = http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte("{}")))
+	c.Request.AddCookie(
+		&http.Cookie{
+			Name:     "sessionid",
+			Value:    "xyz",
+			Path:     "/",
+			MaxAge:   3600,
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+		})
+	
+
+	LoginRequired(c)
+
+	if w.Code != 200 {
+		t.Error(w.Code)
+	}
+}
